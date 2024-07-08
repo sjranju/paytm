81a99c7edf5f7bea6e4ccb36f8c8f8ac9ae50e26
sjranju <sjranju@gmail.com>
2024-07-08 23:08:06 +0530

    Updated proxy in package.json

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -1,13 +1,13 @@
-import express from 'express';
+import Express from 'express';
 import serverless from 'serverless-http';
 import router from '../../routes/index.js'
 import Cors from 'cors';
 
 export async function handler(event, context) {
-    const app = express();
+    const app = Express();
     // Add CORS headers to allow everything
     app.use(Cors());
-    app.use(express.json())
+    app.use(Express.json())
     app.use('/api/v1', router);
     return serverless(app)(event, context);
 };
