f52066387e05e5724c50abdb2c3973542f134d21
sjranju <sjranju@gmail.com>
2024-07-09 12:38:08 +0530

    updated cors in backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,16 @@ import accountRouter from './accountRouter.js'
 
 const router = Router()
 
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allows all domains
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 router.use('/user', userRouter)
 router.use('/account', accountRouter)
+
+
 
 export default router
