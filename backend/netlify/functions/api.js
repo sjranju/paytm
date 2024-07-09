db2788e3deed99543a666bc2f8066b27d7152082
sjranju <sjranju@gmail.com>
2024-07-09 12:39:21 +0530

    updated allow cors headers in backend

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -7,6 +7,12 @@ export async function handler(event, context) {
     const app = Express();
     // Add CORS headers to allow everything
     app.use(Cors());
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*'); // Allows all domains
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    });
     app.use(Express.json())
     app.use('/api/v1', router);
     return serverless(app)(event, context);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,13 +4,6 @@ import accountRouter from './accountRouter.js'
 
 const router = Router()
 
-router.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allows all domains
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
 router.use('/user', userRouter)
 router.use('/account', accountRouter)
 
