2a788fa70408cceb9b2067927a83e5413b0906ac
sjranju <sjranju@gmail.com>
2024-07-09 12:55:58 +0530

    Updated cors allow in app.use

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -1,18 +1,16 @@
 import Express from 'express';
 import serverless from 'serverless-http';
 import router from '../../routes/index.js'
-import Cors from 'cors';
+import cors from 'cors';
 
 export async function handler(event, context) {
     const app = Express();
     // Add CORS headers to allow everything
-    app.use(Cors());
-    app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*'); // Allows all domains
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type');
-        next();
-    });
+    app.use(cors({
+        origin: '*', // Allows all origins
+        methods: 'GET,PUT,POST,DELETE,OPTIONS', // Allowed methods
+        allowedHeaders: 'Content-Type,Authorization' // Allowed headers
+    }));
     app.use(Express.json())
     app.use('/api/v1', router);
     return serverless(app)(event, context);
