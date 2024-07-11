6f133bca3f3c60f1daef1debbf248e9b01b6849e
sjranju <sjranju@gmail.com>
2024-07-11 14:32:58 +0530

    Updated cors options

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -7,24 +7,17 @@ const app = express();
 
 // Add CORS headers to allow everything
 const corsOptions = {
-    origin: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    origin: 'https://payment-application.netlify.app/',
     credentials: true,
     preflightContinue: true,
     optionSuccessStatus: 200,
-    allowedHeaders: [
-        "Origin",
-        "Content-Type",
-        "Accept",
-        "Authorization",
-        "X-Request-With",
-    ],
 };
 
+app.options('*', cors(corsOptions))
 app.use(cors(corsOptions))
-    .options('*', (req, res) => res.send("X-Preflight-Response", "true")).end()
+
 // Handle preflight requests
-// app.options('*', (req, res) => res.set('headers', HEADERS).status(204).send())
+// app.options('*', (req, res) => res.set(corsOptions).status(204).send())
 
 // Middleware to handle JSON requests
 app.use(express.json());
